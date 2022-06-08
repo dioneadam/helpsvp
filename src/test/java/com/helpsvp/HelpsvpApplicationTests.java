3459dbf1bc830db4a1fb2da2c1d43473c6e52ea6
Dione Adam <dione.addm@gmail.com>
2022-06-08 02:09:48 -0300

    add env var to db and config to heroku

--- src/test/java/com/helpsvp/HelpsvpApplicationTests.java
+++ src/test/java/com/helpsvp/HelpsvpApplicationTests.java
@@ -1,13 +1,9 @@
 package com.helpsvp;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HelpsvpApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
